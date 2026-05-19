08a8e78301e7b5e248e11eacfa830047d2ce4a1d
Carlos Eduardo Cabrera Miranda <131503924+CarlosEduardoCabreraMiranda@users.noreply.github.com>
2026-05-19 01:27:22 -0500

    Update Django settings for production
    Set DEBUG to False and configure ALLOWED_HOSTS and STATIC_ROOT.

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--$-8*-t!y0=74y=mg-%oj_)^&(gtk!jr=h^#n0xt=(-mf1xazs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,9 +114,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
